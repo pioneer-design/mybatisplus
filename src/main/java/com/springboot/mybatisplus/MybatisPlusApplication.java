3c061c47620f90899f2478c2dcdb69b688df7638
Albert-del <15596655770@163.com>
2022-08-13 00:28:12 +0800

    修改启动项目类名

--- src/main/java/com/springboot/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/com/springboot/mybatisplus/MybatisPlusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisplusApplication {
+public class MybatisPlusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MybatisplusApplication.class, args);
+		SpringApplication.run(MybatisPlusApplication.class, args);
 	}
 
 }
